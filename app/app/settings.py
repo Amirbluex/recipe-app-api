f2ec293f220fb556f7ffb5f79b65795f1b91b5c6
Amir <amirbluex@gmail.com>
2023-09-17 20:00:10 +0330

    End of section 11

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
     'rest_framework',
-    'drf_spectacular'
+    'drf_spectacular',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -133,5 +133,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'core.User'
 
 REST_FRAMEWORK = {
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openai.AutoSchema',    
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',    
 }
