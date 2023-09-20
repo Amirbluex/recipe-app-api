d986533c3ac967c6d73af6091b8983847112eef7
Amir <amirbluex@gmail.com>
2023-09-20 20:38:54 +0330

    user api error 2

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -8,11 +8,11 @@ from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
     """Serializer for the user object."""
-    
+
     class Meta:
         model = get_user_model()
         fields = ['email', 'password', 'name']
-        extra_kwargs = {'password': {'write only': True, 'min_length': 5}}
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
     def create(self, validated_data):
         """Create and return a user with encrypted password."""
